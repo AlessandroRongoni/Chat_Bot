7264233a22a87bf08dc92dc8cfd61e768ac233df
Alessandro Rongoni <74723429+AlessandroRongoni@users.noreply.github.com>
2024-03-25 18:58:07 +0900

    netflixbot_v2

--- actions/actions.py
+++ actions/actions.py
@@ -14,49 +14,53 @@ import pandas as pd
 PATH_TO_CSV = './dataset/cleaned/netflix_titles_cleaned.csv'
 
 mappatura_generi_italiano = {
-    'Classic Movies': 'Classici',
-    'Anime Series': 'Serie Anime',
-    'TV Horror': 'Serie Horror',
-    'TV Mysteries': 'Gialli e Misteri',
-    'Children & Family Movies': 'Film per Bambini e Famiglia',
-    'Stand-Up Comedy': 'Comici Stand-Up',
-    'Faith & Spirituality': 'Fede e Spiritualità',
-    'TV Comedies': 'Commedie TV',
-    'TV Dramas': 'Drammi TV',
-    'Science & Nature TV': 'Scienza e Natura',
-    'Horror Movies': 'Film Horror',
-    'Action & Adventure': 'Azione e Avventura',
-    'Movies': 'Film',
-    'Sports Movies': 'Film Sportivi',
-    'Crime TV Shows': 'Serie Crime',
-    'Documentaries': 'Documentari',
-    'Stand-Up Comedy & Talk Shows': 'Comici e Talk Show',
-    'Korean TV Shows': 'Serie TV Coreane',
-    'TV Shows': 'Serie TV',
-    'Music & Musicals': 'Musica e Musical',
-    'Romantic TV Shows': 'Serie TV Romantiche',
-    'Teen TV Shows': 'Serie TV per Adolescenti',
-    'Cult Movies': 'Film di Culto',
-    'International Movies': 'Film Internazionali',
-    'Dramas': 'Drammatici',
-    'British TV Shows': 'Serie TV Britanniche',
-    'Thrillers': 'Thriller',
-    'TV Action & Adventure': 'Azione e Avventura TV',
-    'Anime Features': 'Film Anime',
-    'LGBTQ Movies': 'Film LGBTQ+',
-    'Sci-Fi & Fantasy': 'Fantascienza e Fantasy',
-    'TV Thrillers': 'Thriller TV',
-    'Classic & Cult TV': 'TV Classica e di Culto',
-    'International TV Shows': 'Serie TV Internazionali',
-    "Kids' TV": 'TV per Bambini',
-    'Spanish-Language TV Shows': 'Serie TV in Spagnolo',
-    'Comedies': 'Commedie',
-    'Romantic Movies': 'Film Romantici',
-    'Reality TV': 'Reality',
-    'Independent Movies': 'Film Indipendenti',
-    'Docuseries': 'Serie Documentari',
-    'TV Sci-Fi & Fantasy': 'Sci-Fi e Fantasy TV'
+    'Classic Movies': 'classici',
+    'Anime Series': 'anime',
+    'TV Horror': 'horror',
+    'TV Mysteries': 'mistero',
+    'Children & Family Movies': 'famiglia',
+    'Stand-Up Comedy': 'stand-up',
+    'Faith & Spirituality': 'spiritualità',
+    'TV Comedies': 'commedia',
+    'TV Dramas': 'drammatico',
+    'Science & Nature TV': 'scienza e natura',
+    'Horror Movies': 'horror',
+    'Action & Adventure': 'azione e avventura',
+    'Movies': 'film',
+    'Sports Movies': 'sportivi',
+    'Crime TV Shows': 'crime',
+    'Documentaries': 'documentari',
+    'Stand-Up Comedy & Talk Shows': 'commedia e talk show',
+    'Korean TV Shows': 'coreani',
+    'TV Shows': 'serie tv',
+    'Music & Musicals': 'musical',
+    'Romantic TV Shows': 'romantico',
+    'Teen TV Shows': 'adolescenziali',
+    'Cult Movies': 'cult',
+    'International Movies': 'internazionali',
+    'Dramas': 'drammatici',
+    'British TV Shows': 'britannici',
+    'Thrillers': 'thriller',
+    'TV Action & Adventure': 'azione e avventura',
+    'Anime Features': 'anime',
+    'LGBTQ Movies': 'lgbtq',
+    'Sci-Fi & Fantasy': 'fantascienza e fantasy',
+    'TV Thrillers': 'thriller',
+    'Classic & Cult TV': 'classici e cult',
+    'International TV Shows': 'internazionali',
+    "Kids' TV": 'per bambini',
+    'Spanish-Language TV Shows': 'in spagnolo',
+    'Comedies': 'commedia',
+    'Romantic Movies': 'romantici',
+    'Reality TV': 'reality',
+    'Independent Movies': 'indipendenti',
+    'Independent Movies': 'indie',
+    'Docuseries': 'docu-serie',
+    'TV Sci-Fi & Fantasy': 'fantascienza',
+    'TV Sci-Fi & Fantasy': 'fantasy'
 }
+
+
 
 
 class ActionRicercaPerAttore(Action):
@@ -64,51 +68,162 @@ class ActionRicercaPerAttore(Action):
     def name(self) -> Text:
         return "action_ricerca_per_attore"
 
-    async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-        
+    async def run(self, dispatcher, tracker, domain):
         attore = tracker.get_slot('attore')
+        tipo = tracker.get_slot('tipo')
+        df = pd.read_csv(PATH_TO_CSV)
         
-        df = pd.read_csv(PATH_TO_CSV)  # Assicurati che il percorso sia corretto
-        # Cerca attore nel campo 'Cast'
-        risultati = df[df['Cast'].str.contains(attore, case=False, na=False)]
-        
-        if risultati.empty:
-            dispatcher.utter_message(text=f"Mi dispiace, non ho trovato film con l'attore {attore}.")
+        if tipo == "film" or tipo == "movie" or tipo == "titoli":
+            risultati = df[(df['cast'].str.contains(attore, case=False, na=False)) & (df['type'] == "Movie")]
+        elif tipo == "serie TV" or tipo == "tv show" or tipo == "tv" or tipo == "serie":
+            risultati = df[(df['cast'].str.contains(attore, case=False, na=False)) & (df['type'] == "TV Show")]
         else:
-            film = ", ".join(risultati['Titolo'].tolist())
-            dispatcher.utter_message(text=f"Ecco alcuni film con l'attore {attore}: {film}")
+            # Fallback se il tipo non è specificato
+            risultati = df[df['cast'].str.contains(attore, case=False, na=False)]
+
+        if risultati.empty:
+            dispatcher.utter_message(text=f"Mi dispiace, non ho trovato {tipo} con l'attore {attore}.")
+        else:
+            titoli = ",\n ".join(risultati['title'].tolist())
+            dispatcher.utter_message(text=f"Ecco alcuni {tipo} con l'attore {attore}: {titoli}")
 
         return []
 
 
-
-
-
-
 class ActionRicercaPerGenere(Action):
+    
+        def name(self) -> Text:
+            return "action_ricerca_per_genere"
+    
+        async def run(self, dispatcher, tracker, domain):
+            genere = tracker.get_slot('genere')
+            tipo = tracker.get_slot('tipo')
+            df = pd.read_csv(PATH_TO_CSV)
+            genere = mappatura_generi_italiano.get(genere, genere)
+    
+            if tipo == "film" or tipo == "movie" or tipo == "titoli":
+                risultati = df[(df['genres_list'].str.contains(genere, case=False, na=False)) & (df['type'] == "Movie")]
+            elif tipo == "serie TV" or tipo == "tv show" or tipo == "tv" or tipo == "serie":
+                risultati = df[(df['genres_list'].str.contains(genere, case=False, na=False)) & (df['type'] == "TV Show")]
+            else:
+                # Fallback se il tipo non è specificato
+                risultati = df[df['genres_list'].str.contains(genere, case=False, na=False)]
+    
+            if risultati.empty:
+                dispatcher.utter_message(text=f"Mi dispiace, non ho trovato {tipo} del genere {genere}.")
+            else:
+                titoli = ",\n ".join(risultati['title'].tolist())
+                dispatcher.utter_message(text=f"Ecco alcuni {tipo} del genere {genere}: {titoli}")
+    
+            return []
 
+class ActionRicercaPerPaese(Action):
+    
+        def name(self) -> Text:
+            return "action_ricerca_per_paese"
+    
+        async def run(self, dispatcher, tracker, domain):
+            paese = tracker.get_slot('paese')
+            tipo = tracker.get_slot('tipo')
+            df = pd.read_csv(PATH_TO_CSV)
+    
+            if tipo == "film" or tipo == "movie" or tipo == "titoli":
+                risultati = df[(df['country'].str.contains(paese, case=False, na=False)) & (df['type'] == "Movie")]
+            elif tipo == "serie TV" or tipo == "tv show" or tipo == "tv" or tipo == "serie":
+                risultati = df[(df['country'].str.contains(paese, case=False, na=False)) & (df['type'] == "TV Show")]
+            else:
+                # Fallback se il tipo non è specificato
+                risultati = df[df['country'].str.contains(paese, case=False, na=False)]
+    
+            if risultati.empty:
+                dispatcher.utter_message(text=f"Mi dispiace, non ho trovato {tipo} del paese {paese}.")
+            else:
+                titoli = ",\n ".join(risultati['title'].tolist())
+                dispatcher.utter_message(text=f"Ecco alcuni {tipo} del paese {paese}: {titoli}")
+    
+            return []
+        
+class ActionRicercaPerAnno(Action):
+        
+            def name(self) -> Text:
+                return "action_ricerca_per_anno"
+        
+            async def run(self, dispatcher, tracker, domain):
+                anno = tracker.get_slot('anno')
+                tipo = tracker.get_slot('tipo')
+                df = pd.read_csv(PATH_TO_CSV)
+        
+                if tipo == "film" or tipo == "movie" or tipo == "titoli":
+                    risultati = df[(df['release_year'] == int(anno)) & (df['type'] == "Movie")]
+                elif tipo == "serie TV" or tipo == "tv show" or tipo == "tv" or tipo == "serie":
+                    risultati = df[(df['release_year'] == int(anno)) & (df['type'] == "TV Show")]
+                else:
+                    # Fallback se il tipo non è specificato
+                    risultati = df[df['release_year'] == int(anno)]
+        
+                if risultati.empty:
+                    dispatcher.utter_message(text=f"Mi dispiace, non ho trovato {tipo} dell'anno {anno}.")
+                else:
+                    titoli = ",\n ".join(risultati['title'].tolist())
+                    dispatcher.utter_message(text=f"Ecco alcuni {tipo} dell'anno {anno}: {titoli}")
+        
+                return []
+            
+class ActionRicercaPerRegista(Action):
+        
+            def name(self) -> Text:
+                return "action_ricerca_per_regista"
+        
+            async def run(self, dispatcher, tracker, domain):
+                regista = tracker.get_slot('regista')
+                tipo = tracker.get_slot('tipo')
+                df = pd.read_csv(PATH_TO_CSV)
+        
+                if tipo == "film" or tipo == "movie" or tipo == "titoli":
+                    risultati = df[(df['director'].str.contains(regista, case=False, na=False)) & (df['type'] == "Movie")]
+                elif tipo == "serie TV" or tipo == "tv show" or tipo == "tv" or tipo == "serie":
+                    risultati = df[(df['director'].str.contains(regista, case=False, na=False)) & (df['type'] == "TV Show")]
+                else:
+                    # Fallback se il tipo non è specificato
+                    risultati = df[df['director'].str.contains(regista, case=False, na=False)]
+        
+                if risultati.empty:
+                    dispatcher.utter_message(text=f"Mi dispiace, non ho trovato {tipo} diretti dal regista {regista}.")
+                else:
+                    titoli = ",\n ".join(risultati['title'].tolist())
+                    dispatcher.utter_message(text=f"Ecco alcuni {tipo} diretti dal regista {regista}: {titoli}")
+                
+                return []
+
+         
+class ActionRicercaPerTitolo(Action):
+    
     def name(self) -> Text:
-        return "action_ricerca_per_genere"
+        return "action_ricerca_per_titolo"
 
-    async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-        
-        genere_italiano = tracker.get_slot('genere')
-        # Converti il genere dall'italiano all'inglese usando la mappatura
-        genere_inglese = mappatura_generi_italiano.get(genere_italiano, genere_italiano)  # Usa il valore italiano come fallback
-        
-        df = pd.read_csv(PATH_TO_CSV)  # Assicurati che il percorso sia corretto
-        # Cerca genere in inglese nel campo 'Genere'
-        risultati = df[df['Genere'].str.contains(genere_inglese, case=False, na=False)]
-        
+    async def run(self, dispatcher, tracker, domain):
+        titolo_ricerca = tracker.get_slot('titolo')
+        df = pd.read_csv(PATH_TO_CSV)
+        risultati = df[df['title'].str.contains(titolo_ricerca, case=False, na=False)]
+
         if risultati.empty:
-            dispatcher.utter_message(text=f"Mi dispiace, non ho trovato film di genere {genere_italiano}.")
+            dispatcher.utter_message(text=f"Mi dispiace, non ho trovato informazioni su {titolo_ricerca}.")
         else:
-            film = ", ".join(risultati['Titolo'].tolist())
-            dispatcher.utter_message(text=f"Ecco alcuni film di genere {genere_italiano}: {film}")
+            # Formatta i risultati per mostrare informazioni specifiche in modo compatto
+            info_formatted = []
+            for _, row in risultati.iterrows():
+                titolo = row['title']
+                genere = row['genres_list']
+                anno = row['release_year']
+                descrizione = row['description'][:150] + "..." if len(row['description']) > 150 else row['description']
+                
+                # Costruisci il messaggio
+                info = f"**Titolo**: {titolo}\n**Genere**: {genere}\n**Anno**: {anno}\n**Descrizione**: {descrizione}\n"
+                info_formatted.append(info)
+            
+            # Unisci tutte le informazioni in un singolo messaggio
+            message = "\n".join(info_formatted)
+            dispatcher.utter_message(text=f"Ecco alcune informazioni che ho trovato su {titolo_ricerca}:\n{message}")
 
         return []
 
